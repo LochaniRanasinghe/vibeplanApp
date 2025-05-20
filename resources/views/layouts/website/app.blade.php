9a84bab7207e4204d5180f489feec1f251ef8831
LochaniRanasinghe <lochaniransinghe2000@gmail.com>
2025-05-20 16:52:04 +0530

    Add favicon link to the head section of the layout

--- resources/views/layouts/website/app.blade.php
+++ resources/views/layouts/website/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>VibePlan - @yield('title')</title>
+    <link rel="shortcut icon" href="{{ URL::asset('images/vibe-plan-logo.ico') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     {{-- Google Fonts --}}
